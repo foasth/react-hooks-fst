e6b87bcd92e371007563f88aad2fc0d5c65f00fb
foasth <ankhoud@yahoo.fr>
2021-05-21 16:28:07 +0100

    kouceila

--- src/components/moviecard/moviecard.js
+++ src/components/moviecard/moviecard.js
@@ -6,6 +6,7 @@ const MovieCard = (props) => {
     const {movieprops} = props
     return (
         <Card className={'h-card'}>
+            
             <span>Title : {movieprops.title} </span>
             <span>Description : {movieprops.description} </span>
             <span>PosterUrl : {movieprops.posterurl}</span>
